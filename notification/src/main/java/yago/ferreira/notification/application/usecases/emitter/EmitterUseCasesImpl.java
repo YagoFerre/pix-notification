04f4a586b5976d630cfb07e66f7c09599df70782
yagof <yagofer12@gmail.com>
2025-09-11 22:58:14 -0300

    EmitterUseCasesImpl, theres a lot of work to do in this implementation because i did not abstract the SsEmitter correct...
    But maybe should i abstract this? i think no... but i should understand this

--- notification/src/main/java/yago/ferreira/notification/application/usecases/emitter/EmitterUseCasesImpl.java
+++ notification/src/main/java/yago/ferreira/notification/application/usecases/emitter/EmitterUseCasesImpl.java
@@ -5,15 +5,42 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import yago.ferreira.notification.domain.model.SseEmitterModel;
 import yago.ferreira.notification.domain.port.out.usecases.SseEmitterUseCase;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class EmitterUseCasesImpl implements SseEmitterUseCase {
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
     @Override
     public SseEmitter addEmitter(Long userId) {
-        return null;
+        SseEmitter emitter = new SseEmitter();
+        emitters.put(userId, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(userId));
+        emitter.onTimeout(() -> emitters.remove(userId));
+        emitter.onError((e) -> {
+            emitters.remove(userId);
+            throw new RuntimeException("Erro ao abrir SseEmitter" + e.getMessage());
+        });
+
+        return emitter;
     }
 
     @Override
-    public SseEmitterModel publishNotification(Long userId, SseEmitterModel sseEmitterModel) {
-        return null;
+    public SseEmitterModel publishNotification(Long userId, SseEmitterModel sseEmitterModel) throws IOException {
+        SseEmitter emitter = emitters.get(userId);
+
+        if (emitter == null) {
+            throw new RuntimeException("Emitter nao encontrado com o id " + userId);
+        }
+
+        try {
+            emitter.send(SseEmitter.event().data(sseEmitterModel));
+            return sseEmitterModel;
+        } catch (IOException ioException) {
+            throw new IOException("Error ao publicar pix ao SSE " + ioException.getMessage());
+        }
     }
 }
--- notification/src/main/java/yago/ferreira/notification/domain/port/out/usecases/SseEmitterUseCase.java
+++ notification/src/main/java/yago/ferreira/notification/domain/port/out/usecases/SseEmitterUseCase.java
@@ -3,11 +3,13 @@ package yago.ferreira.notification.domain.port.out.usecases;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import yago.ferreira.notification.domain.model.SseEmitterModel;
 
+import java.io.IOException;
+
 /**
  * Um abre conexao, outro vai servir de axilio
  * para o RabbitMq ao escutar a fila
  */
 public interface SseEmitterUseCase {
     SseEmitter addEmitter(Long userId);
-    SseEmitterModel publishNotification(Long userId, SseEmitterModel sseEmitterModel);
+    SseEmitterModel publishNotification(Long userId, SseEmitterModel sseEmitterModel) throws IOException;
 }
