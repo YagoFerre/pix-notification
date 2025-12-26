ab8aade18398371f211eeacfadf0fb3f578e742b
yagof <yagofer12@gmail.com>
2025-12-25 21:50:00 -0300

    SseEmitter is not a thread safe, so i have done a new Map<Long, Object> emittersLocks = new ConcurrentHashMap<>(); because the emitters.send() can crash if heartbeat and publishMessage concurrent the same time

--- notification/src/main/java/yago/ferreira/notification/adapters/in/service/EmitterService.java
+++ notification/src/main/java/yago/ferreira/notification/adapters/in/service/EmitterService.java
@@ -21,17 +21,22 @@ public class EmitterService implements SseEmitterHandler {
      * Container que armazena as conexoes abertas
      */
     private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>(); // sessions: {"key": 2, "value: "emitter", "key": 34, "value: "emitter"}
-    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final Map<Long, Object> emittersLocks = new ConcurrentHashMap<>();
+
+    private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
     public SseEmitter openEmitterClient(Long userId) {
+        closeExistingConnections(userId);
+
         SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
         emitters.put(userId, emitter);
+        emittersLocks.put(userId, new Object());
 
         emitter.onCompletion(() -> emitters.remove(userId));
         emitter.onTimeout(() -> emitters.remove(userId));
         emitter.onError((e) -> {
             emitters.remove(userId);
-            throw new RuntimeException("Erro ao abrir SseEmitter" + e.getMessage());
+            log.error("Erro ao abrir SseEmitter {}", e.getMessage());
         });
 
         return emitter;
@@ -40,14 +45,21 @@ public class EmitterService implements SseEmitterHandler {
     @Override
     public void publishNotification(SseEmitterResponse sseEmitterResponse) {
         SseEmitter emitter = emitters.get(sseEmitterResponse.getId());
+        Object lock = emittersLocks.get(sseEmitterResponse.getId());
+
+        if (lock == null) return;
 
         if (emitter != null) {
             // executa a tarefa de enviar uma notification em uma thread separada
             executor.execute(() -> {
-                try {
-                    emitter.send(SseEmitter.event().data(sseEmitterResponse));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                synchronized (lock) {
+                    try {
+                        emitter.send(SseEmitter.event().data(sseEmitterResponse));
+                    } catch (IOException e) {
+                        log.error("Error to publish message: {}", e.getMessage());
+                        emitters.remove(sseEmitterResponse.getId());
+                        emittersLocks.remove(sseEmitterResponse.getId());
+                    }
                 }
             });
 
@@ -55,10 +67,27 @@ public class EmitterService implements SseEmitterHandler {
     }
 
     /*
-     * Sends to user every 30 secs, and remove dead emitters
+     * The current user can not have twice connections at the same time
+     * even in another window browser
      */
-    @Scheduled(fixedRate = 30000)
-    public void sendHeartbeat() {
+    private void closeExistingConnections(Long userId) {
+        SseEmitter existingEmitter = emitters.get(userId);
+
+        if (existingEmitter != null) {
+            existingEmitter.complete(); // clean existing opened emitter user
+            emitters.remove(userId);
+            emittersLocks.remove(userId);
+
+            log.warn("Successfully! to close exiting emitter for userId: {}", userId);
+        }
+    }
+
+
+    /*
+     * Sends to user every 10 secs, and remove dead emitters
+     */
+    @Scheduled(fixedRate = 10000)
+    private void sendHeartbeat() {
         if (emitters.isEmpty()) {
             return;
         }
@@ -66,24 +95,30 @@ public class EmitterService implements SseEmitterHandler {
         log.info("Sending heartbeat to {} active connections", emitters.size());
 
         emitters.forEach((userId, emitter) -> {
+            Object lock = emittersLocks.get(userId);
+            if (lock == null) return;
+
             // separate thread for execute this task
             executor.execute(() -> {
-                try {
-                    emitter.send(SseEmitter.event().comment("ping"));
-                    log.info("💓 Heartbeat sent to userId: {}", userId);
-                } catch (IOException e) {
-                    log.warn("Heartbeat failed for userId: {}, connection is dead", userId);
-                    emitters.remove(userId);
-                    log.info("Successfully to remove userId: {} dead connection", userId);
-                } catch (IllegalStateException e) {
-                    log.warn("Heartbeat failed for userId: {}, emitter in illegal state", userId);
-                    emitters.remove(userId);
-                    log.info("Successfully to remove userId: {}", userId);
+                synchronized (lock) {
+                    try {
+                        emitter.send(SseEmitter.event().comment("ping"));
+                        log.info("💓 Heartbeat sent to userId: {}", userId);
+                    } catch (IOException e) {
+                        log.error("Heartbeat failed for userId: {}, connection is dead", userId);
+                        emitters.remove(userId);
+                        emittersLocks.remove(userId);
+                        log.error("Successfully to remove userId: {} dead connection", userId);
+                    } catch (IllegalStateException e) {
+                        log.error("Heartbeat failed for userId: {}, emitter in illegal state", userId);
+                        emitters.remove(userId);
+                        emittersLocks.remove(userId);
+                        log.error("Successfully to remove userId: {}", userId);
+                    }
                 }
             });
+
         });
 
     }
-
-
 }
